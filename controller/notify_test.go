4cf85419128978c8e07a8eaeae23becef78bbae6
shoubhik bose <sbose78@gmail.com>
2017-12-21 16:18:56 +0530

    add more tests for template

--- controller/notify_test.go
+++ controller/notify_test.go
@@ -3,13 +3,17 @@ package controller
 import (
 	"context"
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 
 	"github.com/fabric8-services/fabric8-notification/app"
 	"github.com/fabric8-services/fabric8-notification/app/test"
 	"github.com/fabric8-services/fabric8-notification/collector"
+	"github.com/fabric8-services/fabric8-notification/configuration"
 	"github.com/fabric8-services/fabric8-notification/email"
 	"github.com/fabric8-services/fabric8-notification/template"
+	"github.com/fabric8-services/fabric8-notification/wit"
 	"github.com/goadesign/goa"
 )
 
@@ -31,4 +35,37 @@ func TestNotifySendUnknownType(t *testing.T) {
 	}
 
 	test.SendNotifyBadRequest(t, nil, nil, ctrl, payload)
+}
+
+func TestNotifySendWithCustomParam(t *testing.T) {
+
+	config, _ := configuration.GetData()
+
+	resolvers := &collector.LocalRegistry{}
+	typeRegistry := &template.AssetRegistry{}
+	witClient, _ := wit.NewCachedClient(config.GetWITURL())
+
+	resolvers.Register("user.email.update", collector.ConfiguredVars(config, collector.NewUserResolver(witClient)))
+	notifier := &email.CallbackNotifier{
+		Callback: func(ctx context.Context, notification email.Notification) {
+			require.NotNil(t, notification.CustomAttributes)
+			assert.Equal(t, notification.CustomAttributes["verifyURL"], "https://someurl.openshift.io")
+		}}
+
+	ctrl := NewNotifyController(goa.New("send-test"), resolvers, typeRegistry, notifier)
+
+	payload := &app.SendNotifyPayload{
+		Data: &app.Notification{
+			Attributes: &app.NotificationAttributes{
+				ID:   "13132",
+				Type: "user.email.update",
+				Custom: map[string]interface{}{
+					"verifyURL": "https://someurl.openshift.io",
+				},
+			},
+			Type: "notifications",
+		},
+	}
+
+	test.SendNotifyAccepted(t, nil, nil, ctrl, payload)
 }
